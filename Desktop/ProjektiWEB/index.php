2742be4dbb6fa6b545df9272ce9d5aad742edeea
OltiM4 <149538501+OltiM4@users.noreply.github.com>
2024-02-01 13:31:18 +0100

    Ndrrimi i fileve nga .html ne .php

---
+++ Desktop/ProjektiWEB/index.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OMEK</title>
+    <link rel="icon" href="logoOmek.png" type="images/png">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Karla:wght@400;700&family=Gill+Sans:wght@400;700&display=swap">
+
+
+</head>
+<body>
+    
+    <header>
+        <div class="headeri">
+            <a href="index.html">
+            <img src="logoOmek.png" alt="file:///C:/desktop/ProjektiWEB/index.html#">
+        </a>
+        </div>
+        <ul>
+            <li><a href="#" onclick="gotoHome()">Home</a></li>
+            <li><a href="#" onclick="gotoAboutUs()">About Us</a></li>
+            <li><a href="news.html" target="_blank">News</a></li>
+            <li><a href="#" onclick="gotoContactUs()">Contact Us</a></li>
+            <li><a href="products.html" target="_blank">Products</a></li>
+
+            <li><a href="login.html" target="_blank">Log In</a></li>
+        </ul>
+    </header>
+
+        <style>
+            button {
+                font-size: 18px;
+                color: #222;
+                font-family: inherit;
+                font-weight: 800;
+                cursor: pointer;
+                position: relative;
+                border: none;
+                background: none;
+                text-transform: uppercase;
+                transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+                transition-duration: 400ms;
+                transition-property: color;
+            }
+    
+            button:focus,
+            button:hover {
+                color: #222;
+            }
+    
+            button:focus:after,
+            button:hover:after {
+                width: 100%;
+                left: 0%;
+            }
+    
+            button:after {
+                content: "";
+                pointer-events: none;
+                bottom: -2px;
+                left: 50%;
+                position: absolute;
+                width: 0%;
+                height: 2px;
+                background-color: black;
+                transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+                transition-duration: 400ms;
+                transition-property: width, left;
+            }
+        </style>
+    </head>
+    <body>
+        <button>
+          <h1>WELCOME TO OMEK</h1>
+        </button>
+  
+    
+    <div class="slider-frame">
+        <div class="slide-images">
+                <div class="img-container">
+                    <img src="banner4.jpg">
+                </div>
+                <div class="img-container">
+                    <img src="fotobanner.jpg">
+                </div>
+                <div class="img-container">
+                    <img src="banner2.jpg">
+                </div>
+                <div class="img-container">
+                    <img src="banner3.jpg">
+                </div>
+                
+        </div>
+    </div>
+    <div class="storyline">
+        <button>
+    <p>Dive into the universe of OMEK, an Albanian fashion brand ignited by the vivid pulse of youth culture,
+         the ever-evolving trends of street style, and the raw energy of the urban landscape. Founded in 2015,
+          the brand's official launch in 2017 marked the inception of a remarkable journey. 
+          Fueled solely by a dream and an unwavering ambition, OMEK has rapidly expanded its footprint,
+           offering worldwide shipping and establishing physical retail havens across three distinct national markets.</p>
+        </button>
+        </div>   
+           <main>
+                <p></p>
+            </div>
+            <div class="foto">
+        </div>
+     </div>
+
+     <div class="maincontainer2">
+        <div class="aboutus" id="aboutus">
+            <div class="fototekst"> 
+                <h1>Our Story</h1>
+            </div>
+            </div>
+            <div class="tekstd">
+                <button>
+                <p>OMEK is an Albanian fashion brand inspired by vibrant youth culture, trends, street style and the urban world. The clothing brand was founded in 2015 and launched in 2017 and quickly expanded, currently offering worldwide shipment and physical retail stores on three different national markets. Furthermore, a well thought-out range of unisex as well as male and female clothing.
+
+
+
+                    All I had was a dream and ambition...  </p>
+                </button>
+                </div>
+
+     </div>
+
+<div class="space2"></div>
+<style>
+    button {
+        font-size: 18px;
+        color: #222;
+        font-family: inherit;
+        font-weight: 800;
+        cursor: pointer;
+        position: relative;
+        border: none;
+        background: none;
+        text-transform: uppercase;
+        transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+        transition-duration: 400ms;
+        transition-property: color;
+    }
+
+    button:focus,
+    button:hover {
+        color: #222;
+    }
+
+    button:focus:after,
+    button:hover:after {
+        width: 100%;
+        left: 0%;
+    }
+
+    button:after {
+        content: "";
+        pointer-events: none;
+        bottom: -2px;
+        left: 50%;
+        position: absolute;
+        width: 0%;
+        height: 2px;
+        background-color: black;
+        transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+        transition-duration: 400ms;
+        transition-property: width, left;
+    }
+</style>
+</head>
+<body>
+
+    </main>
+
+    <div class="space3"></div>
+    <footer class="footer">
+            <div class="contactus" id="contactus">
+                <div class="row">
+                    <div class="footer-col">
+                        <ul>
+                        <dl type = "circle">
+                            <li> <h4>Company</h4></li>
+                            <li><a href="#">About us</a></li> 
+                            <li><a href="#">Our services</a></li>
+                            <li><a href="#">Privacy policy</a></li>
+                            <li><a href="#">Affiliate program</a></li>
+                        </dl>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <ul>
+                        <dl type = "circle">
+                            <li><h4>Get help</h4></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Shipping</a></li>
+                            <li><a href="#">Returns</a></li>
+                            <li><a href="#">Order status</a></li>
+                            <li><a href="#">Payment options</a></li>
+                        </dl>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <ul>
+                            <dl type = "circle">
+                                <li><a href="file:///C:/Users/HP/Desktop/ProjektiWEB-master/products.html"><h4>Online shop</h4></a></li>
+                            <li><a href="#">Jackets</a></li>
+                            <li><a href="#">Watch</a></li>
+                            <li><a href="#">Shoes</a></li>
+                            <li><a href="#">Parfume</a></li>
+                            </dl>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>Follow us</h4>
+                        <div class="social-links">
+                            <a href=""><img src="facebook.png" alt=""></a>
+                            <a href=""><img src="twitter.png" alt=""></a>
+                            <a href=""><img src="instagram.png" alt=""></a>
+                            <a href=""><img src="pinterest.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </footer>
+     </body>
+     </html>
+
+       <script>
+        function gotoHome() {
+            var homeSection = document.getElementById("home");
+            if (homeSection) {
+                homeSection.scrollIntoView({ behavior: "smooth" });
+            }
+       }
+
+       function gotoAboutUs() {
+        var aboutusSection = document.getElementById("aboutus");
+        if(aboutusSection) {
+            aboutusSection.scrollIntoView({ behavior: "smooth"});
+        }
+       }
+
+       function gotoNews() {
+        var productsSection = document.getElementById("news");
+        if(productsSection) {
+            productsSection.scrollIntoView({behavior:"smooth"});
+        }
+       }
+
+       function gotoContactUs() {
+        var contactSection = document.getElementById("contactus");
+        if(contactSection) {
+            contactSection.scrollIntoView({behavior:"smooth"});
+        }
+       }
+
+
+    </script>
+      
+
+      
