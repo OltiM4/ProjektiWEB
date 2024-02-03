f4e08fe30daff9c09a936832c4a9df1fea8668ff
OltiM4 <149538501+OltiM4@users.noreply.github.com>
2024-02-03 20:01:27 +0100

    Shtimi i siderbar

---
+++ dashboardComponents/actionsSidebar.php
@@ -0,0 +1,16 @@
+<div class="sidebar" id="mySidebar">
+<div class="side-header">
+    <h3 style="margin-top:10px;">Hello, Admin</h3>
+</div>
+
+<hr style="border:1px solid; background-color:#8a7b6d; border-color:#3B3131;">
+    <a href="../pages/index.php" ><i class="fa fa-home"></i> Dashboard</a>
+    <a href="usersDashboard.php"  onclick="showCustomers()" ><i class="fa fa-users"></i> Users</a>
+    <a href="bookingsDashboard.php"   onclick="showCategory()" ><i class="fa fa-calendar"></i> Purchases</a>
+  
+  
+</div>
+ 
+<div id="main">
+    <button class="openbtn" onclick="openNav()"><i class="fa fa-home"></i></button>
+</div>
